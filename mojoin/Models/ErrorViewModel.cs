886d450ccb06cfb414fc5f866ded349e2b83244e
Louis Phan <imthanq7@outlook.com>
2023-04-12 23:22:40 +0700

    Add project files.

---
+++ mojoin/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace mojoin.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
