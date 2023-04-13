b27ad03759faeae3485e4523e4590bf648df1099
Vulea <Vulea@LAPTOP-P4D3L99G>
2023-04-13 15:02:54 +0700

    design layout

---
+++ mojoin/Controllers/Dangtin.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace mojoin.Controllers
+{
+    public class Dangtin : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
