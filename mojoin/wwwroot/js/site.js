44ea5e2f3b272f455521132918a39ad933149520
Vulea <Vulea@LAPTOP-P4D3L99G>
2023-04-14 21:58:28 +0700

    hhhjgh

--- mojoin/wwwroot/js/site.js
+++ mojoin/wwwroot/js/site.js
@@ -2,3 +2,12 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+var myDiv = document.getElementById("open");
+
+myDiv.addEventListener("click", function () {
+    if (myDiv.style.display === "none") {
+        myDiv.style.display = "block";
+    } else {
+        myDiv.style.display = "none";
+    }
+});
