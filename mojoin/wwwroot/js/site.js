bde7be6cfbf1c002542fc654b7f6d0fa430939e3
Vulea <Vulea@LAPTOP-P4D3L99G>
2023-04-22 22:16:36 +0700

    layout

--- mojoin/wwwroot/js/site.js
+++ mojoin/wwwroot/js/site.js
@@ -11,3 +11,30 @@ myDiv.addEventListener("click", function () {
         myDiv.style.display = "none";
     }
 });
+//button prev and next
+var prevButton = document.getElementById("prevButton");
+var nextButton = document.getElementById("nextButton");
+
+prevButton.addEventListener("click", function () {
+    history.back();
+});
+
+nextButton.addEventListener("click", function () {
+    history.forward();
+});
+//like and unlike
+(function () {
+    const btn = document.getElementById("btn_save_ad");
+    const img = document.getElementById("img_save_ad");
+
+    btn.addEventListener("click", () => {
+        if (img.getAttribute("src") === "https://static.chotot.com/storage/icons/saveAd/save-ad.svg") {
+            img.setAttribute("src", "https://static.chotot.com/storage/icons/saveAd/save-ad-active.svg");
+        } else {
+            img.setAttribute("src", "https://static.chotot.com/storage/icons/saveAd/save-ad.svg");
+        }
+    });
+})();
+
+
+
