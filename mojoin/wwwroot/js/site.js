609c1b328a42f50a3cd090ca9cea4f0daa4fc119
Vulea <Vulea@LAPTOP-P4D3L99G>
2023-04-23 22:27:31 +0700

    like

--- mojoin/wwwroot/js/site.js
+++ mojoin/wwwroot/js/site.js
@@ -2,39 +2,35 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-var myDiv = document.getElementById("open");
+//var myDiv = document.getElementById("open");
 
-myDiv.addEventListener("click", function () {
-    if (myDiv.style.display === "none") {
-        myDiv.style.display = "block";
-    } else {
-        myDiv.style.display = "none";
-    }
-});
+//myDiv.addEventListener("click", function () {
+//    if (myDiv.style.display === "none") {
+//        myDiv.style.display = "block";
+//    } else {
+//        myDiv.style.display = "none";
+//    }
+//});
 //button prev and next
-var prevButton = document.getElementById("prevButton");
-var nextButton = document.getElementById("nextButton");
+//var prevButton = document.getElementById("prevButton");
+//var nextButton = document.getElementById("nextButton");
 
-prevButton.addEventListener("click", function () {
-    history.back();
-});
+//prevButton.addEventListener("click", function () {
+//    history.back();
+//});
 
-nextButton.addEventListener("click", function () {
-    history.forward();
-});
+//nextButton.addEventListener("click", function () {
+//    history.forward();
+//});
 //like and unlike
-(function () {
-    const btn = document.getElementById("btn_save_ad");
-    const img = document.getElementById("img_save_ad");
+//const btn = document.getElementById("btn_save_ad");
+//const img = document.getElementById("img_save_ad");
 
-    btn.addEventListener("click", () => {
-        if (img.getAttribute("src") === "https://static.chotot.com/storage/icons/saveAd/save-ad.svg") {
-            img.setAttribute("src", "https://static.chotot.com/storage/icons/saveAd/save-ad-active.svg");
-        } else {
-            img.setAttribute("src", "https://static.chotot.com/storage/icons/saveAd/save-ad.svg");
-        }
-    });
-})();
-
-
+//btn.addEventListener("click", () => {
+//    if (img.getAttribute("src") === "https://static.chotot.com/storage/icons/saveAd/save-ad.svg") {
+//        img.setAttribute("src", "https://static.chotot.com/storage/icons/saveAd/save-ad-active.svg");
+//    } else {
+//        img.setAttribute("src", "https://static.chotot.com/storage/icons/saveAd/save-ad.svg");
+//    }
+//});
 
