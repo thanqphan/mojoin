075be969d6d58751ef16b48a642d5f0a9ba7b327
Louis Phan <imthanq7@outlook.com>
2023-05-18 23:35:51 +0700

    Dang Bai + design

--- mojoin/Controllers/MyAccountController.cs
+++ mojoin/Controllers/MyAccountController.cs
@@ -1,9 +1,16 @@
 ﻿using AspNetCoreHero.ToastNotification.Abstractions;
+using AspNetCoreHero.ToastNotification.Notyf;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using mojoin.Models;
+using mojoin.ViewModel;
+using System.Security.Claims;
 
 namespace mojoin.Controllers
 {
+    [Authorize]
     public class MyAccountController : Controller
     {
         private readonly DbmojoinContext _context;
@@ -17,5 +24,75 @@ namespace mojoin.Controllers
         {
             return View();
         }
+        [HttpGet]
+        [AllowAnonymous]
+        [Route("dang-bai.html", Name = "DangBai")]
+        public IActionResult CreatePosts()
+        {
+            var roomTypes = _context.RoomTypes.ToList();
+            ViewBag.RoomTypes = roomTypes;
+            return View();
+        }
+        [HttpPost]
+        [AllowAnonymous]
+        [Route("dang-bai.html", Name = "DangBai")]
+        public async Task<IActionResult> CreatePosts(Room room)
+        {
+            var roomTypes = _context.RoomTypes.ToList();
+            ViewBag.RoomTypes = roomTypes;
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    //*						string salt = Utilities.GetRandomKey();
+                    Room user = new Room
+                    {
+                        RoomTypeId = room.RoomTypeId,
+                        UserId = room.UserId,
+                        Title = room.Title,
+                        Description = room.Description,
+                        Price = room.Price,
+                        Area = room.Area,
+                        NumRooms = room.NumRooms,
+                        NumBathrooms = room.NumBathrooms,
+                        CreateDate = DateTime.Now,
+                        LastUpdate = DateTime.Now,
+                        IsActive = 0,
+                        StreetNumber = room.StreetNumber,
+                        Street = room.Street,
+                        Ward = room.Ward,
+                        District = room.District,
+                        City = room.City,
+                        HasAirConditioner = room.HasAirConditioner,
+                        HasElevator = room.HasElevator,
+                        HasParking = room.HasParking,
+                        HasRefrigerator = room.HasRefrigerator,
+                        HasWasher = room.HasWasher,
+                        ViewCount = 1,
+                    };
+                    try
+                    {
+                        _context.Add(room);
+                        await _context.SaveChangesAsync();
+
+                        _notyfService.Success("Gửi bài thành công!");
+                        return RedirectToAction("Index", "MyAccount");
+                    }
+                    catch
+                    {
+                        return RedirectToAction("Register", "Account");
+                    }
+                }
+                else
+                {
+                    return View(room);
+                }
+            }
+            catch
+            {
+                return View(room);
+            }
+        }
     }
+
 }
---
+++ mojoin/Controllers/Rooms1Controller.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using mojoin.Models;
+
+namespace mojoin.Controllers
+{
+    public class Rooms1Controller : Controller
+    {
+        private readonly DbmojoinContext _context;
+
+        public Rooms1Controller(DbmojoinContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Rooms1
+        public async Task<IActionResult> Index()
+        {
+            var dbmojoinContext = _context.Rooms.Include(r => r.RoomType).Include(r => r.User);
+            return View(await dbmojoinContext.ToListAsync());
+        }
+
+        // GET: Rooms1/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Rooms == null)
+            {
+                return NotFound();
+            }
+
+            var room = await _context.Rooms
+                .Include(r => r.RoomType)
+                .Include(r => r.User)
+                .FirstOrDefaultAsync(m => m.RoomId == id);
+            if (room == null)
+            {
+                return NotFound();
+            }
+
+            return View(room);
+        }
+
+        // GET: Rooms1/Create
+        public IActionResult Create()
+        {
+            ViewData["RoomTypeId"] = new SelectList(_context.RoomTypes, "RoomTypeId", "RoomTypeId");
+            ViewData["UserId"] = new SelectList(_context.Users, "UserId", "UserId");
+            return View();
+        }
+
+        // POST: Rooms1/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("RoomId,RoomTypeId,UserId,Title,Description,Price,Area,NumRooms,NumBathrooms,CreateDate,LastUpdate,IsActive,StreetNumber,Street,Ward,District,City,HasRefrigerator,HasAirConditioner,HasWasher,HasElevator,HasParking,ViewCount")] Room room)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(room);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["RoomTypeId"] = new SelectList(_context.RoomTypes, "RoomTypeId", "RoomTypeId", room.RoomTypeId);
+            ViewData["UserId"] = new SelectList(_context.Users, "UserId", "UserId", room.UserId);
+            return View(room);
+        }
+
+        // GET: Rooms1/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Rooms == null)
+            {
+                return NotFound();
+            }
+
+            var room = await _context.Rooms.FindAsync(id);
+            if (room == null)
+            {
+                return NotFound();
+            }
+            ViewData["RoomTypeId"] = new SelectList(_context.RoomTypes, "RoomTypeId", "RoomTypeId", room.RoomTypeId);
+            ViewData["UserId"] = new SelectList(_context.Users, "UserId", "UserId", room.UserId);
+            return View(room);
+        }
+
+        // POST: Rooms1/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("RoomId,RoomTypeId,UserId,Title,Description,Price,Area,NumRooms,NumBathrooms,CreateDate,LastUpdate,IsActive,StreetNumber,Street,Ward,District,City,HasRefrigerator,HasAirConditioner,HasWasher,HasElevator,HasParking,ViewCount")] Room room)
+        {
+            if (id != room.RoomId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(room);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!RoomExists(room.RoomId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["RoomTypeId"] = new SelectList(_context.RoomTypes, "RoomTypeId", "RoomTypeId", room.RoomTypeId);
+            ViewData["UserId"] = new SelectList(_context.Users, "UserId", "UserId", room.UserId);
+            return View(room);
+        }
+
+        // GET: Rooms1/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Rooms == null)
+            {
+                return NotFound();
+            }
+
+            var room = await _context.Rooms
+                .Include(r => r.RoomType)
+                .Include(r => r.User)
+                .FirstOrDefaultAsync(m => m.RoomId == id);
+            if (room == null)
+            {
+                return NotFound();
+            }
+
+            return View(room);
+        }
+
+        // POST: Rooms1/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Rooms == null)
+            {
+                return Problem("Entity set 'DbmojoinContext.Rooms'  is null.");
+            }
+            var room = await _context.Rooms.FindAsync(id);
+            if (room != null)
+            {
+                _context.Rooms.Remove(room);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool RoomExists(int id)
+        {
+          return (_context.Rooms?.Any(e => e.RoomId == id)).GetValueOrDefault();
+        }
+    }
+}
