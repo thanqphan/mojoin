a596328245aa8ee488b057bf5f4ea885a7cb1647
Ngan <huynhthitrucngantl@gmail.com>
2023-06-03 14:15:28 +0700

    hohohoohhh

--- mojoin/ViewModel/LoginViewModel.cs
+++ mojoin/ViewModel/LoginViewModel.cs
@@ -2,18 +2,18 @@
 
 namespace mojoin.ViewModel
 {
-	public class LoginViewModel
-	{
-		[Key]
-		[MaxLength(100)]
-		[Required(ErrorMessage = ("Vui lòng nhập SĐT"))]
-		[Display(Name = "Email or SDT")]
-		[Phone(ErrorMessage = "Sai định dạng")]
-		public string UserName { get; set; }
+    public class LoginViewModel
+    {
+        [Key]
+        [MaxLength(100)]
+        [Required(ErrorMessage = ("Vui lòng nhập SĐT"))]
+        [Display(Name = "Email or SDT")]
+        [RegularExpression(@"^(0|\+84)[3-9]\d{8}$", ErrorMessage = "Sai định dạng Số điện thoại")]
+        public string UserName { get; set; }
 
-		[Display(Name = "Mật khẩu")]
-		[Required(ErrorMessage = "Vui lòng nhập mật khẩu")]
-		[MinLength(5, ErrorMessage = "Bạn cần đặt mật khẩu tối thiểu 5 ký tự")]
-		public string Password { get; set; }
-	}
+        [Display(Name = "Mật khẩu")]
+        [Required(ErrorMessage = "Vui lòng nhập mật khẩu")]
+        [MinLength(5, ErrorMessage = "Bạn cần nhập mật khẩu tối thiểu 5 ký tự")]
+        public string Password { get; set; }
+    }
 }
--- mojoin/ViewModel/RegisterViewModel.cs
+++ mojoin/ViewModel/RegisterViewModel.cs
@@ -3,24 +3,26 @@ using System.ComponentModel.DataAnnotations;
 
 namespace mojoin.ViewModel
 {
-	public class RegisterViewModel
-	{
-		[Key]
-		public int CustomerId { get; set; }
+    public class RegisterViewModel
+    {
+        [Key]
+        public int CustomerId { get; set; }
 
-		[Display(Name = "Họ")]
-		[Required(ErrorMessage = "Vui lòng nhập Họ Tên")]
-		public string LastName { get; set; }
-        [Display(Name = "Tên")]
+        [Display(Name = "Họ")]
         [Required(ErrorMessage = "Vui lòng nhập Họ Tên")]
+        [RegularExpression(@"^[^0-9]+$", ErrorMessage = "Họ và tên không chứa sô!")]
+
+        public string LastName { get; set; }
+        [Display(Name = "Tên")]
+        [RegularExpression(@"^[^0-9]+$", ErrorMessage = "Họ và tên không chứa sô!")]
         public string FirstName { get; set; }
 
         [MaxLength(11)]
-		[Required(ErrorMessage = "Vui lòng nhập số điện thoại")]
-		[Display(Name = "Điện thoại")]
-		[DataType(DataType.PhoneNumber)]
-		[Remote(action: "ValidatePhone", controller: "Login")]
-		public string Phone { get; set; }
+        [Required(ErrorMessage = "Vui lòng nhập số điện thoại")]
+        [Display(Name = "Điện thoại")]
+        [DataType(DataType.PhoneNumber)]
+        [Remote(action: "ValidatePhone", controller: "Login")]
+        public string Phone { get; set; }
 
         [MaxLength(150)]
         [Required(ErrorMessage = "Vui lòng nhập Email")]
@@ -29,13 +31,14 @@ namespace mojoin.ViewModel
         public string Email { get; set; }
 
         [Display(Name = "Mật khẩu")]
-		[Required(ErrorMessage = "Vui lòng nhập mật khẩu")]
-		[MinLength(5, ErrorMessage = "Bạn cần đặt mật khẩu tối thiểu 5 ký tự")]
-		public string Password { get; set; }
+        [Required(ErrorMessage = "Vui lòng nhập mật khẩu")]
+        [MinLength(5, ErrorMessage = "Bạn cần đặt mật khẩu tối thiểu 5 ký tự")]
+        public string Password { get; set; }
 
-		[MinLength(5, ErrorMessage = "Bạn cần đặt mật khẩu tối thiểu 5 ký tự")]
-		[Display(Name = "Nhập lại mật khẩu")]
-		[Compare("Password", ErrorMessage = "Nhập lại mật khẩu không đúng")]
-		public string ConfirmPassword { get; set; }
-	}
+        [Required(ErrorMessage = "Vui lòng xác nhận mật khẩu")]
+        [MinLength(5, ErrorMessage = "Bạn cần đặt mật khẩu tối thiểu 5 ký tự")]
+        [Display(Name = "Nhập lại mật khẩu")]
+        [Compare("Password", ErrorMessage = "Nhập lại mật khẩu không đúng")]
+        public string ConfirmPassword { get; set; }
+    }
 }
