c047236e0dac79995a5e580a394efeb233929fbc
Thang Phan <phanthq2k2@gmail.com>
2023-12-16 01:22:29 +0700

    ngaan merged into master

--- mojoin/Models/Room.cs
+++ mojoin/Models/Room.cs
@@ -54,9 +54,6 @@ public partial class Room
     public int? DisplayType { get; set; }
 
     public string? Video { get; set; }
-	
-    //public string? RejectionReason { get; set; }
-
 	public virtual ICollection<RoomFavorite> RoomFavorites { get; set; } = new List<RoomFavorite>();
 
     public virtual ICollection<RoomImage> RoomImages { get; set; } = new List<RoomImage>();
