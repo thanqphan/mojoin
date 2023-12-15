d9b1f5058b8e8cda9c006934d1a0d40c160ad2e8
phucl <phucl@LOK>
2023-12-16 00:49:08 +0700

    ls

--- mojoin/Areas/Admin/Controllers/AdminController.cs
+++ mojoin/Areas/Admin/Controllers/AdminController.cs
@@ -1,12 +1,42 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AspNetCoreHero.ToastNotification.Abstractions;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using mojoin.Models;
+
 
 namespace mojoin.Areas.Admin.Controllers
 {
+    [Area("Admin")]
+    [Authorize(Policy = "AdminOrStaff")]
     public class AdminController : Controller
     {
+        private readonly DbmojoinContext _context;
+
+        public AdminController(DbmojoinContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Admin/TransactionHistory
         public IActionResult Index()
         {
-            return View();
+            var history = _context.TransactionHistories.ToList();
+            List<SelectListItem> lstrangthai = new List<SelectListItem>();
+            lstrangthai.Add(new SelectListItem() { Text = "  Thành công ", Value = "0" });
+            lstrangthai.Add(new SelectListItem() { Text = "Thất bại", Value = "1" });
+            lstrangthai.Add(new SelectListItem() { Text = "Đang xử lý", Value = "2" });
+
+
+            ViewData["lstrangthaihoatdongg"] = lstrangthai;
+            return View(history);
         }
     }
 }
+
+
